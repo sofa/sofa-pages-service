07604e0d92270a4204128d1993796e80ac484cfc
Pascal Precht <pascal.precht@googlemail.com>
2014-03-24 18:49:43 +0100

    chore(*): releases 0.1.0

---
+++ dist/sofa.pagesService.js
@@ -0,0 +1,75 @@
+/**
+ * sofa-pages-service - v0.1.0 - 2014-03-24
+ * 
+ *
+ * Copyright (c) 2013 CouchCommerce GmbH (http://www.couchcommerce.org) and other contributors
+ * THIS SOFTWARE CONTAINS COMPONENTS OF THE SOFA SDK (SOFA.IO).
+ * IT IS PROVIDED UNDER THE LICENSE TERMS OF THE ATTACHED LICENSE.TXT.
+ */
+;(function (sofa, undefined) {
+
+'use strict';
+/* global sofa */
+/**
+ * @name PagesService
+ * @namespace sofa.PagesService
+ *
+ * @description
+ * This service takes care of accessing static page data.
+ */
+sofa.define('sofa.PagesService', function ($http, $q, configService) {
+
+    var self = {};
+
+    var RESOURCE_URL = configService.get('resourceUrl') + 'html/',
+        ABOUT_PAGES  = configService.get('aboutPages');
+
+    /**
+     * @method getPage
+     * @memberof sofa.PagesService
+     *
+     * @description
+     * Returns a page object by a given id.
+     *
+     * @param {int} id Page id.
+     * @return {object} Page object.
+     */
+    self.getPage = function (id) {
+        return $http({
+            method: 'get',
+            url: RESOURCE_URL + id + '.html'
+        }).then(function (result) {
+            if (result.data) {
+
+                //we don't want to directly alter the page config, so we create a copy
+                var pageConfig = sofa.Util.clone(self.getPageConfig(id));
+
+                pageConfig.content = result.data;
+
+                return pageConfig;
+            }
+        });
+    };
+
+    /**
+     * @method getPageConfig
+     * @memberof sofa.PagesService
+     *
+     * @description
+     * Returns a page configuration object by a given page id.
+     *
+     * @param {int} id Page id.
+     * @return {object} Page configuration
+     */
+    self.getPageConfig = function (id) {
+        var page = ABOUT_PAGES.filter(function (page) {
+            return page.id === id;
+        });
+
+        return page.length > 0 && page[0];
+    };
+
+    return self;
+});
+
+} (sofa));
---
+++ test/sofa.pagesService.spec.js
@@ -0,0 +1,88 @@
+'use strict';
+/* global sofa */
+/* global AsyncSpec */
+
+describe('sofa.pagesService', function () {
+
+    var pagesService, q, httpService, configService;
+    var id = 'saturn';
+
+    var createHttpService = function () {
+        return new sofa.mocks.httpService(new sofa.QService());
+    };
+
+    beforeEach(function () {
+        q = new sofa.QService();
+        configService = new sofa.ConfigService();
+        httpService = new sofa.HttpService(q);
+        pagesService = new sofa.PagesService(httpService, q, configService);
+    });
+
+    it('should be defined', function () {
+        expect(pagesService).toBeDefined();
+    });
+
+    it('should be an object', function () {
+        expect(typeof pagesService).toBe('object');
+    });
+
+    it('should have a method getPage', function () {
+        expect(pagesService.getPage).toBeDefined();
+    });
+
+    it('should have a method getPageConfig', function () {
+        expect(pagesService.getPageConfig).toBeDefined();
+    });
+
+    describe('sofa.pagesService#getPage', function () {
+
+        var async = new AsyncSpec(this),
+            httpService,
+            pagesService;
+
+
+        beforeEach(function () {
+            httpService = createHttpService();
+            pagesService = new sofa.PagesService(httpService, q, configService);
+        });
+
+        it('should be a function', function () {
+            expect(typeof pagesService.getPage).toBe('function');
+        });
+
+        async.it('should fetch page content via http', function (done) {
+
+            httpService.when('get', configService.get('resourceUrl') + 'html/' + id + '.html')
+                .respond({
+                    data: 'bar'
+                });
+
+            pagesService.getPage(id).then(function (config) {
+                expect(typeof config).toBe('object');
+                expect(config.title).toBeDefined();
+                expect(config.id).toBeDefined();
+                expect(config.id).toEqual('saturn');
+                done();
+            });
+
+        });
+    });
+
+    describe('sofa.pagesService#getPageConfig', function () {
+
+        it('should be a function', function () {
+            expect(typeof pagesService.getPageConfig).toBe('function');
+        });
+
+        it('should return an object', function () {
+            expect(typeof pagesService.getPageConfig(id)).toBe('object');
+        });
+
+        it('should return page configuration by given id', function () {
+            var config = pagesService.getPageConfig(id);
+            expect(config.title).toBeDefined();
+            expect(config.id).toBeDefined();
+            expect(config.id).toEqual('saturn');
+        });
+    });
+});
